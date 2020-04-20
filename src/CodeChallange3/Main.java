e53137144e3bc5a35a457f892991ad1dfcf2182f
Alexandra Glentoaica <ag.alle@yahoo.ro>
2020-04-20 15:54:11 +0300

    Insufficient data added before-Code Challenge 4 fixed

--- src/CodeChallange3/Main.java
+++ src/CodeChallange3/Main.java
@@ -5,20 +5,20 @@ import java.util.Arrays;
 public class Main {
     static int contor_perechi = 0;
     static int[] intArray; //Vectorul de genul [5,9,-5,7,-5];
-    static int [] boolArray; //Vectorul boolean corespondent celui de int-uri care initial are forma [false,false,false,false,false];
+    static boolean [] boolArray; //Vectorul boolean corespondent celui de int-uri care initial are forma [false,false,false,false,false];
 
     public static void main(String[] args) {
 
-        intArray=new int[]{5,9,-5,7,-5,5};
-        boolArray=new int[intArray.length]; //In functie de numarul de int-uri pe care il introducem in tabloul principal vom crea si dimensiunea pentru tabloul bool;
+        intArray=new int[]{5,9,-5,7,-5};
+        boolArray=new boolean[intArray.length]; //In functie de numarul de int-uri pe care il introducem in tabloul principal vom crea si dimensiunea pentru tabloul bool;
 
-        Arrays.fill(boolArray, 0);
+        Arrays.fill(boolArray, false);
         for(int i=0;i<intArray.length;i++){
             for(int j=i+1;j<intArray.length;j++){
-               if(intArray[i]+intArray[j]==0 && boolArray[i]==0 && boolArray[j]==0 ) {
+               if(intArray[i]+intArray[j]==0 && !boolArray[i] && !boolArray[j]) {
                    contor_perechi++;
-                   boolArray[i]=1;
-                   boolArray[j]=1;
+                   boolArray[i]=true;
+                   boolArray[j]=true;
                }
             }
         }
--- src/CodeChallange4/Main.java
+++ src/CodeChallange4/Main.java
@@ -1,4 +1,37 @@
 package CodeChallange4;
 
+import java.util.Arrays;
+
 public class Main {
+    static int contor_perechi=0;
+    static int[] int_array;
+    static boolean[] boolean_array;
+
+    public static void main(String[] args) {
+        int_array=new int[]{-1,-2,-1,2};
+        boolean_array=new boolean[int_array.length];
+
+        Arrays.fill(boolean_array, false);
+
+        for (int i = 0; i < int_array.length; i++)
+        {
+            for (int j = i + 1; j < int_array.length; j++)
+            {
+                for (int k = j + 1; k < int_array.length; k++)
+                {
+                    if ((int_array[i] + int_array[j] + int_array[k] == 0) && (boolean_array[i] == false) && (boolean_array[j] == false) && (boolean_array[k] == false))
+                    {
+                        boolean_array[i] = true;
+                        boolean_array[j] = true;
+                        boolean_array[k] = true;
+                        contor_perechi++;
+                    }
+                }
+            }
+        }
+        System.out.print(contor_perechi);
+    }
+
+        
 }
+
