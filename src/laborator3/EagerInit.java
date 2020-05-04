fe42296c5c8515cc06ed82aa96154311459cc0f6
Alexandra Glentoaica <ag.alle@yahoo.ro>
2020-05-04 16:58:53 +0300

    Eager Singleton class

---
+++ src/laborator3/EagerInit.java
@@ -0,0 +1,12 @@
+package laborator3;
+
+public class EagerInit {
+    private static final EagerInit instance=new EagerInit();
+    private EagerInit() {} //Constructorul este gol pentru a nu putea crea alt obiect de acest tip;
+
+    public static EagerInit getInstance(){
+        return instance;
+    }
+
+
+}
