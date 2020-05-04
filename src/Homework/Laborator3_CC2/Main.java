46d83b029e6ff678fce041f5d5ffb9377dce970a
Alexandra Glentoaica <ag.alle@yahoo.ro>
2020-05-04 17:07:00 +0300

    Laborator 3 - CodeChallange2 done

---
+++ src/Homework/Laborator3_CC2/Main.java
@@ -0,0 +1,28 @@
+package Homework.Laborator3_CC2;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void displaySorted(SortingStrategy strategy,Integer[] arr)
+    {
+        strategy.sort(arr);
+
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+ " ");
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        Integer[] arr=new Integer[]{1,4,5,2,3,6,9,8,7};
+        Integer[] copyArr1= Arrays.copyOf(arr,arr.length);
+        Integer[] copyArr2=Arrays.copyOf(arr,arr.length);
+
+        displaySorted(new MergeSort(),copyArr1);
+        System.out.println();
+        displaySorted(new BubbleSort(),copyArr2);
+    }
+
+}
---
+++ src/Homework/Laborator3_CC2/SortingStrategy.java
@@ -0,0 +1,5 @@
+package Homework.Laborator3_CC2;
+
+public interface SortingStrategy {
+    void sort(Integer[] list);
+}
